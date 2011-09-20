aefcf10972332d44c98b6763a84f874280dcc3cc
Mikkel Krautz <mikkel@krautz.dk>
2011-09-20 11:08:05 +0200

    Fetch crypto/openpgp from the Go repo. It's not currently present on App Engine.

--- clean-deps.bash
+++ clean-deps.bash
@@ -3,3 +3,5 @@
 rm -rf github.com
 rm -rf appengine-go-backports
 rm -rf gorilla.googlecode.com
+rm -rf crypto
+rm -rf go # just in case
--- fetch-deps.bash
+++ fetch-deps.bash
@@ -7,3 +7,8 @@ git clone https://github.com/mkrautz/goar github.com/mkrautz/goar
 hg clone https://appengine-go-backports.googlecode.com/hg appengine-go-backports
 hg clone -u go.r58 https://gorilla.googlecode.com/ gorilla.googlecode.com/hg
 rm -rf gorilla.googlecode.com/hg/examples
+
+hg clone -u release.r58.1 https://go.googlecode.com/hg/go go
+mkdir -p crypto
+mv go/src/pkg/crypto/openpgp crypto/
+rm -rf go
